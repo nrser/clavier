7693e772746790ddaf7f22f0daae5964a5babeb5
nrser <neil@neilsouza.com>
2023-02-04 18:04:28 -0800

    refactor async console a bit

--- clavier/embed/async_embedded_console.py
+++ clavier/embed/async_embedded_console.py
@@ -1,6 +1,7 @@
 from argparse import Namespace
 import asyncio
 from collections import defaultdict
+from contextlib import contextmanager
 import signal
 from typing import Any, Callable, Mapping, Protocol, TypeAlias
 import sys
@@ -47,6 +48,7 @@ class AsyncEmbeddedConsole:
     _out: Console
     _parser: arg_par.ArgumentParser
     _done: bool = False
+    _args: Namespace | None = None
 
     def __init__(
         self,
@@ -86,66 +88,6 @@ class AsyncEmbeddedConsole:
 
         self._log.debug("Done reading input.", read_task=self._read_task)
 
-    async def _run(self, args: Namespace):
-        if not hasattr(args, "__target__"):
-            self._log.error("Missing __target__ arg", args=args)
-            raise err.InternalError("Missing __target__ arg")
-
-        # Form the call keyword args -- start with a dict of the parsed arguments
-        kwds = {**args.__dict__}
-        # Remove the global argument names
-        for key in self._parser.action_dests():
-            if key in kwds:
-                del kwds[key]
-        # And the `__target__` that holds the target function
-        del kwds["__target__"]
-
-        # Resolve default getters
-        _resolve_default_getters(kwds)
-
-        # pylint: disable=broad-except
-        try:
-            if asyncio.iscoroutinefunction(unwrap(args.__target__)):
-                result = await args.__target__(**kwds)
-            else:
-                result = args.__target__(**kwds)
-        except (asyncio.CancelledError, KeyboardInterrupt):
-            raise
-        except Exception as error:
-            if getattr(args, "backtrace", None):
-                self._log.error(
-                    "[holup]Terminating due to unhandled exception[/holup]...",
-                    exc_info=True,
-                )
-            else:
-                self._log.error(
-                    "Command [uhoh]FAILED[/uhoh].\n\n"
-                    f"{type(error).__name__}: {error}\n\n"
-                    "Add `--backtrace` to print stack.",
-                )
-            return
-
-        if not isinstance(result, io.View):
-            result = io.View(result)
-
-        try:
-            result.render(args.output)
-        except (asyncio.CancelledError, KeyboardInterrupt):
-            raise
-        except Exception as error:
-            if getattr(args, "backtrace", None):
-                self._log.error(
-                    "[holup]Terminting due to view rendering error[/holup]...",
-                    exc_info=True,
-                )
-            else:
-                self._log.error(
-                    "Command [uhoh]FAILED[/uhoh].\n\n"
-                    f"{type(error).__name__}: {error}\n\n"
-                    "Add `--backtrace` to print stack.",
-                )
-            return
-
     async def loop(self):
         self._log.debug("Setting up loop...")
 
@@ -173,13 +115,7 @@ class AsyncEmbeddedConsole:
                     "Got input from queue", input=repr(input), argv=argv
                 )
 
-                if argv:
-                    try:
-                        args = self._parser.parse_args(argv)
-                    except SystemExit as error:
-                        pass
-                    else:
-                        await self._run(args)
+                await self.run(argv)
 
         except (asyncio.CancelledError, KeyboardInterrupt):
             self._done = True
@@ -190,3 +126,115 @@ class AsyncEmbeddedConsole:
                 ev.remove_signal_handler(signal_number)
 
             ev.remove_reader(self._file)
+
+    # The `Sesh`` stuff
+    # ========================================================================
+    #
+    # Things that overlap with `Sesh`, and ideally would be consolidated largely
+    # or completely.
+    #
+
+    def is_backtracing(self) -> bool:
+        if self._args is None:
+            return False
+        return getattr(self._args, "backtrace", False)
+
+    async def run(self, argv: list[str]) -> None:
+        try:
+            await self._run_internal(argv)
+
+        except (asyncio.CancelledError, KeyboardInterrupt) as error:
+            # Bubble this up so the process can exit
+            #
+            self._log.debug("Interupted, re-raising", error=error)
+            raise
+
+        except SystemExit as error:
+            level = splatlog.DEBUG if error.code == 0 else splatlog.ERROR
+
+            self._log.log(
+                level,
+                "System exit during arg parsing",
+                code=error.code,
+                exc_info=sys.exc_info(),
+            )
+
+        except err.InternalError as error:
+            # Internal error (in code / logic) are always printed,
+            # regardless of backtrace setting. It's a developer tool afterall,
+            # and someone needs to fix it.
+            #
+            self._log.exception("`run` encountered an internal error")
+
+        except Exception as error:
+            # Catch-all
+            #
+            if self.is_backtracing():
+                self._log.error(
+                    "[holup]Terminting due to view rendering error[/holup]...",
+                    exc_info=True,
+                )
+            else:
+                self._log.error(
+                    "Command [uhoh]FAILED[/uhoh].\n\n"
+                    f"{type(error).__name__}: {error}\n\n"
+                    "Add `--backtrace` to print stack.",
+                )
+
+    @contextmanager
+    def _args_context(self, args: Namespace):
+        if self._args is not None:
+            raise err.InternalError("`_args` already populated")
+
+        self._args = args
+        try:
+            yield
+        finally:
+            self._args = None
+
+    async def _run_internal(self, argv: list[str]) -> None:
+        if not argv:
+            # Ignore empty lines (user spamming return key)
+            return
+
+        try:
+            args = self._parser.parse_args(argv)
+        except SystemExit as error:
+            level = splatlog.DEBUG if error.code == 0 else splatlog.ERROR
+
+            self._log.log(
+                level,
+                "System exit during arg parsing",
+                code=error.code,
+                exc_info=sys.exc_info(),
+            )
+
+            return
+
+        with self._args_context(args):
+
+            if not hasattr(args, "__target__"):
+                self._log.error("Missing __target__ arg", args=args)
+                raise err.InternalError("Missing __target__ arg")
+
+            # Form the call keyword args -- start with a dict of the parsed arguments
+            kwds = {**args.__dict__}
+            # Remove the global argument names
+            for key in self._parser.action_dests():
+                if key in kwds:
+                    del kwds[key]
+            # And the `__target__` that holds the target function
+            del kwds["__target__"]
+
+            # Resolve default getters
+            _resolve_default_getters(kwds)
+
+            if asyncio.iscoroutinefunction(unwrap(args.__target__)):
+                result = await args.__target__(**kwds)
+            else:
+                result = args.__target__(**kwds)
+
+            if not isinstance(result, io.View):
+                result = io.View(result)
+
+            result.render(args.output)
