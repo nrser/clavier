d04788f71604541fde99eca64ed7fc4f38722bb9
nrser <neil@neilsouza.com>
2023-02-21 00:37:00 -0800

    rm old cfg.extract function (for now)
    see cfg.Config._extract_ for replacement (for now)

--- clavier/cfg/__init__.py
+++ clavier/cfg/__init__.py
@@ -14,14 +14,6 @@ from .container import Container
 from .context import ContextualConfig
 
 T = TypeVar("T")
-_T1 = TypeVar("_T1")
-_T2 = TypeVar("_T2")
-_T3 = TypeVar("_T3")
-_T4 = TypeVar("_T4")
-_T5 = TypeVar("_T5")
-_T6 = TypeVar("_T6")
-_T7 = TypeVar("_T7")
-_T8 = TypeVar("_T8")
 
 SELF_ROOT_KEY = Key(__package__).root
 
@@ -248,55 +240,6 @@ def get_pkg_scope(
     ```
     """
     return get_scope(Key(module_name).root)
-
-
-@overload
-def extract(scope: object, k1: tuple[str, type[_T1]], /) -> tuple[_T1]:
-    ...
-
-
-@overload
-def extract(
-    scope: object, k1: tuple[str, type[_T1]], k2: tuple[str, type[_T2]], /
-) -> tuple[_T1, _T2]:
-    ...
-
-
-@overload
-def extract(
-    scope: object,
-    k1: tuple[str, type[_T1]],
-    k2: tuple[str, type[_T2]],
-    k3: tuple[str, type[_T3]],
-    /,
-) -> tuple[_T1, _T2, _T3]:
-    ...
-
-
-@overload
-def extract(
-    scope: object,
-    k1: tuple[str, type[_T1]],
-    k2: tuple[str, type[_T2]],
-    k3: tuple[str, type[_T3]],
-    k4: tuple[str, type[_T4]],
-    /,
-) -> tuple[_T1, _T2, _T3, _T4]:
-    ...
-
-
-def extract(scope: object, *keys):
-    check_type(scope, Config | Scope, argname="scope")
-    scope = cast(Config | Scope, scope)
-
-    values = []
-
-    for key, type_ in keys:
-        value = scope[key]
-        check_type(value, type_, argname=key)
-        values.append(value)
-
-    return tuple(values)
 
 
 def changeset(*prefix: KeyMatter, **meta: Any) -> Changeset:
